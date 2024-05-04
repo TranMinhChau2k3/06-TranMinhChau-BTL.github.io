3158018e5a63f420c595156ab9611048de6efc99
Tran Minh Chau <chau@gmail.com>
2024-05-04 17:35:24 +0700

    capnhat

--- js/dangky.js
+++ js/dangky.js
@@ -1,9 +1,8 @@
 $(document).ready(function () {
-  let stt = 2; // Khởi tạo số thứ tự từ 2 vì đã có 2 người dùng mẫu trong bảng
-  let userList = []; // Mảng lưu danh sách người dùng
+  let stt = 1;
 
   function checkName(name) {
-    let patternName = /^[A-Z][a-zA-Z\s_-]{2,}$/;
+    let patternName = /^[A-Z][a-z0-9_-]{2,}$/;
     let arrName = name.toString().split(' ');
     let flag = false;
 
@@ -17,32 +16,14 @@ $(document).ready(function () {
   }
 
   function checkEmail(email) {
-    let patternEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    let patternEmail = /^[a-z0-9]{2,}@([a-z]{3,})\.([a-z]{2,})/;
     return patternEmail.test(email);
-}
+  }
 
-function checkPassword(password) {
-// Mẫu regex mới yêu cầu mật khẩu có ít nhất 8 ký tự và bao gồm ít nhất một chữ hoa, một chữ thường và một chữ số
-function checkPassword(password) {
-  let patternPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@$!%*?&]{8,}$/;
-  return patternPassword.test(password);
-}
-
-return patternPassword.test(password);
-}
-
-function addUserToTable(name, email, password) {
-  userList.push({ name: name, email: email, password: password }); // Thêm người dùng vào danh sách
-  $('.table tbody').append(`
-    <tr>
-      <th scope="row">${stt++}</th>
-      <td>${name}</td>
-      <td>${email}</td>
-      <td>${password}</td>
-    </tr>
-  `);
-}
-
+  function checkPassword(password) {
+    let patternPassword = /^[a-z0-9]{2,}@([0-9]{2,})/;
+    return patternPassword.test(password);
+  }
 
   $('#txtName').change(function (e) {
     if (checkName(e.target.value)) {
@@ -58,11 +39,11 @@ function addUserToTable(name, email, password) {
 
   $('#txtEmail').change(function (e) {
     if (checkEmail(e.target.value) === false) {
-      $('.emailErr').text('*Địa chỉ email không hợp lệ');
+      $('.emailErr').text('*Email không hợp lệ');
       $('.emailErr').removeClass('text-success');
       $('.emailErr').addClass('text-danger');
     } else {
-      $('.emailErr').text('*Địa chỉ email hợp lệ');
+      $('.emailErr').text('*Email hợp lệ');
       $('.emailErr').removeClass('text-danger');
       $('.emailErr').addClass('text-success');
     }
@@ -70,7 +51,7 @@ function addUserToTable(name, email, password) {
 
   $('#txtPassword').change(function (e) {
     if (checkPassword(e.target.value) === false) {
-      $('.passwordErr').text('*Mật khẩu bắt buộc theo cú pháp Chau_1234!');
+      $('.passwordErr').text('*Mật khẩu bắt buộc theo cú pháp hello@123');
       $('.passwordErr').removeClass('text-success');
       $('.passwordErr').addClass('text-danger');
     } else {
@@ -93,7 +74,7 @@ function addUserToTable(name, email, password) {
     }
   });
 
-  $('.btn-submit').click(function (e) {
+  $('form').submit(function (e) {
     e.preventDefault();
     let name = $('#txtName').val();
     let email = $('#txtEmail').val();
@@ -106,36 +87,36 @@ function addUserToTable(name, email, password) {
       checkPassword(password) === false ||
       retypePass !== password
     ) {
-      if (checkName(name)) {
-        $('.nameErr').text('*Tên bắt buộc có độ dài ít nhất 2 ký tự và bắt đầu mỗi chữ bằng in hoa');
-      }
-      if (!checkEmail(email)) {
-        $('.emailErr').text('*Địa chỉ email không hợp lệ');
-      }
-      if (!checkPassword(password)) {
-        $('.passwordErr').text('*Mật khẩu bắt buộc theo cú pháp Chau_1234!');
-      }
-      if (retypePass !== password) {
-        $('.retypePassErr').text('*Mật khẩu không trùng khớp');
-      }
       return;
     }
-
-    addUserToTable(name, email, password);
-
-    // Hiển thị modal sau khi thêm người dùng vào bảng
-    $('.myModal').modal('show');
-
-    // Xóa dữ liệu trên form
-    $('#txtName').val('');
-    $('#txtEmail').val('');
-    $('#txtPassword').val('');
-    $('#retypePass').val('');
-    // Xóa thông báo lỗi
+    $('.table tbody').append(`
+		<tr>
+			<th scope="row">${++stt}</th>
+			<td>${name}</td>
+			<td>${email}</td>
+			<td>${password}</td>
+		</tr>
+		`);
+    $('.nameErr').removeClass('text-danger');
+    $('.nameErr').removeClass('text-success');
     $('.nameErr').text('');
+    $('#txtName').val('');
+
+    $('.emailErr').removeClass('text-danger');
+    $('.emailErr').removeClass('text-success');
     $('.emailErr').text('');
+    $('#txtEmail').val('');
+
+    $('.passwordErr').removeClass('text-danger');
+    $('.passwordErr').removeClass('text-success');
     $('.passwordErr').text('');
+    $('#txtPassword').val('');
+
+    $('.retypePassErr').removeClass('text-danger');
+    $('.retypePassErr').removeClass('text-success');
     $('.retypePassErr').text('');
+    $('#retypePass').val('');
+    $('.myModal').modal('hide');
   });
 
   $('.btn-reset').click(function (e) {
